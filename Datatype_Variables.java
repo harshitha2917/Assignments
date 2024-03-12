8692e9a387073ecc02165ee18911e4a0c71619e7
Harshitha B A <harshithaashwath98@gmail.com>
2024-03-13 03:55:03 +0530

    Assignment 3

---
+++ Datatype_Variables.java
@@ -0,0 +1,47 @@
+package basics_of_java;
+
+//Assignment-4
+public class Datatype_Variables {
+
+	public static void main(String[] args) {
+//declaration and initialization of data types an variables
+
+		byte a = 10; // -127 to 128
+		System.out.println(a);
+
+		short b = 32123; // -32768 to 32767
+		System.out.println(b);
+
+		int c = 98765;// 32 bits
+		System.out.println(c);
+
+		long d = 987654;// 64 bits
+		System.out.println(d);
+
+		float e = 5.43f;// decimal value + f
+		System.out.println(e);
+
+		double a1 = 876.432;
+		System.out.println(a1);
+
+		char b1 = 'J';// declare inside single quote
+		System.out.println(b1);
+
+		boolean c1 = true;
+		System.out.println(c1);
+
+		boolean c2 = false;
+		System.out.println(c2);
+
+		String name = "Manish Kumar Tiwari";
+		System.out.println(name);
+
+	}
+}
+/*
+* other way to dec and ini variables
+* 
+* int number1=100; dec and ini
+* 
+* int number2; dec number2=200; ini
+*/
