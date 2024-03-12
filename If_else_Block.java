2ecc3d6e001230eb9d6921486a41aa915c058fdd
Harshitha B A <harshithaashwath98@gmail.com>
2024-03-13 04:04:19 +0530

    Assignment 9

---
+++ If_else_Block.java
@@ -0,0 +1,16 @@
+package conditional_stmts;
+
+//Assignment-9
+public class If_else_Block {
+
+	public static void main(String[] args) {
+		int age = 20;
+		if (age > 18) {
+			System.out.println("Person is eligible to vote");
+		} else {
+			System.out.println("Person is not eligible to vote");
+		}
+
+	}
+
+}
