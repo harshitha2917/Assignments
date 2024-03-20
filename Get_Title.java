febecf7ab41aaf76df5a4411d55d4d6ea4d7e1d3
Harshitha B A <harshithaashwath98@gmail.com>
2024-03-21 02:54:58 +0530

    Assignment 76

---
+++ Get_Title.java
@@ -0,0 +1,15 @@
+package launch.selenium;
+//Assignment-76
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Get_Title {
+
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		String title=driver.getTitle();
+		System.out.println(title);
+
+	}
+
+}
